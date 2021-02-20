c489c635da174487588c44316487298ed905955e
r0ld3x <r0ld3xrahu1@gmail.com>
2021-02-20 07:43:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# TTS-BOT
+A Telegram bot that can Convert Text To Speach
+
+# DEPLOY
+YOU CAN SIMPLY DEPLOY ON HEROKU BY CLICKING THE BUTTON BELOW
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/r0ld3x/MORAG-TEXT-BOT/tree/main)
+
+AFTER DEPLOY DO SET WEBHOOK BY ``api.telegram.org/bot<your bot token>/setwebhook?url=<Heroku URL>``
+
+# Configarations
+
+``TG_BOT_TOKEN : YOUR BOT TOKEN FROM @BOTFATHER``
+
+``START_MESSAGE : A START MESSAGE FOR YOUR BOT``
+
+``BOT_USER_NAME : A START MESSAGE FOR YOUR BOT``
+
+# COMMANDS
+
+``/start - To stat the bot``
+
+
+##
+
+**DO GIVE A STAR TO MY PROJECT TO SHOW YOUR SUPPORT!!**
---
+++ web/index.php
@@ -0,0 +1,60 @@
+<?php
+/*
+CREDIT GOES TO BENCHAMXD
+*/
+require_once __DIR__ . "/config.php";
+
+ob_start();
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+ function sendmessage($chat_id, $text, $model){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ 'text'=>$text,
+ 'parse_mode'=>$mode
+ ]);
+ }
+//==================RITHUNAND [BENCHAMXD]==================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$message_id = $message->message_id;
+$from_id = $message->from->id;
+$chat_id = $message->chat->id;
+$first_name = $message->from->first_name;
+$last_name = $message->from->last_name;
+$username = $message->from->userame;
+$text = $message->text;
+//==================RITHUNAND [BENCHAMXD]==================//
+if($text == '/start'){
+bot('sendmessage',[
+ 'chat_id'=>$chat_id,
+ 'text'=>"$START_MESSAGE",
+'parse_mode'=>"HTML",
+'reply_to_message_id'=>$message_id,
+ ]);
+}
+if($text !== '/start'){
+$indusbots = file_get_contents('http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text='.urlencode($text));
+ file_put_contents('in.ogg',$indusbots);
+ bot('sendvoice',[
+ 'chat_id'=>$chat_id,
+'reply_to_message_id'=>$message_id,
+ 'voice'=>new CURLFile('in.ogg'),
+'caption'=>"$BOT_USER_NAME",
+ ]);
+ unlink('in.ogg');
+ }
+?>
